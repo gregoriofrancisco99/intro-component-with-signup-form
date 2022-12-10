bd446edee1b00962c2597b8520581e5c09d87deb
Gregório Francisco <gregoriofrancisco2014@gmail.com>
2022-12-10 14:13:42 +0100

    Finalize input fields validation

--- assets/js/validateForm.js
+++ assets/js/validateForm.js
@@ -1,39 +1,51 @@
-function formValidator() {
-  var firstName = document.getElementById('firstName').value;
-  var lastName = document.getElementById('lastName').value;
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
+function formValidator(event) {
+  event.preventDefault();
+
+  var firstName = document.querySelector('.textBox.firstName input');
+  var lastName = document.querySelector('.textBox.lastName input');
+  var email = document.querySelector('.textBox.email input');
+  var password = document.querySelector('.textBox.password input');
+
+  console.log(email, password, firstName, lastName);
 
   function emailValidator() {
     console.log('here!')
-    let atPos = email.indexOf('@');
-    let dotPos = email.lastIndexOf('.');
+    let atPos = email.value.indexOf('@');
+    let dotPos = email.value.lastIndexOf('.');
 
     if(atPos < 1 || (dotPos - atPos) < 2) {
       document.querySelector('.textBox.email .alert').innerHTML = 'Looks like this is not an email';
-
-      return false;
+      
+      return false
     }
     document.querySelector('.textArea.email .alert').innerHTML = '';
     return true;
   }
 
-  if (firstName == '') {
-    document.querySelector('.textBox.firstName .alert').innerHTML = 'Name cannot be empty';
+  if (firstName.value == '') {
+    firstName.classList.add('failed');
+    document.querySelector('.textBox.firstName .alert').classList.add('failed');
+    document.querySelector('.textBox.firstName img').classList.add('failed');
   }
-  if (lastName == '') {
-    document.querySelector('.textBox.lastName .alert').innerHTML = 'Name cannot be empty';
+  if (lastName.value == '') {
+    lastName.classList.add('failed');
+    document.querySelector('.textBox.firstName .alert').classList.add('failed');
+    document.querySelector('.textBox.firstName img').classList.add('failed');
   }
   if (email ==! '') {
     emailValidator();
   }
-  else if(email == '') {
-    document.querySelector('.textBox.email .alert').innerHTML = 'Email cannot be empty';
+  else if(email.value == '') {
+    email.classList.add('failed');
+    document.getElementsByName('Email')[0].placeholder='Email cannot be empty';
+
+    document.querySelector('.textBox.email .alert').classList.add('failed');
+    document.querySelector('.textBox.email img').classList.add('failed');
   }
 
-  if (password == '') {
-    document.querySelector('.textBox.password .alert').innerHTML = 'Password cannot be empty';
+  if (password.value == '') {
+    password.classList.add('failed');
+    document.querySelector('.textBox.password .alert').classList.add('failed');
+    document.querySelector('.textBox.password img').classList.add('failed');
   }
-
-  return false;
 }
